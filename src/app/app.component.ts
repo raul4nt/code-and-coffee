fdce8840c98946e4883ec9396f76e2a230c15b4d
Raul Silveira <raulantsilveira@gmail.com>
2025-04-25 16:28:15 -0300

    fix: swiper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { ProductCarouselComponent } from "./components/product-carousel/product-carousel.component";
+import 'swiper/swiper-bundle.min.css';
+
 
 @Component({
   selector: 'app-root',
--- src/app/components/product-carousel/product-carousel.component.ts
+++ src/app/components/product-carousel/product-carousel.component.ts
@@ -42,26 +42,39 @@
 //   }
 // }
 
-import { Component, OnInit }    from '@angular/core';
-import { CommonModule }          from '@angular/common';
-import { SwiperModule }          from 'swiper/angular';
-import { Product }               from '../../models/product.model';
-import { HttpClient }            from '@angular/common/http';
+import { Component, ElementRef, AfterViewInit, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../../models/product.model';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-product-carousel',
   standalone: true,
-  imports: [ CommonModule, SwiperModule ],
+  imports: [CommonModule],
   templateUrl: './product-carousel.component.html',
   styleUrls: ['./product-carousel.component.css']
 })
-export class ProductCarouselComponent implements OnInit {
+export class ProductCarouselComponent implements OnInit, AfterViewInit {
   products: Product[] = [];
+  @ViewChild('swiperContainer', { static: false }) swiperContainer!: ElementRef;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.http.get<Product[]>('http://localhost:3000/products')
       .subscribe(data => this.products = data.slice(0, 10));
+  }
+
+  ngAfterViewInit() {
+    new Swiper(this.swiperContainer.nativeElement, {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      loop: false,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      }
+    });
   }
 }
