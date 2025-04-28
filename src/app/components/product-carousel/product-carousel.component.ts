d6fb3cf367c9c0be3645d30c1d4a06bf41c66045
Raul Silveira <raulantsilveira@gmail.com>
2025-04-28 14:58:36 -0300

    fix: translate to pt-br

--- src/app/components/product-carousel/product-carousel.component.ts
+++ src/app/components/product-carousel/product-carousel.component.ts
@@ -1,47 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { HttpClient }     from '@angular/common/http';
-// import { Product }        from '../../models/product.model';
-
-// @Component({
-//   selector: 'app-product-carousel',
-//   standalone: true,
-//   imports: [CommonModule],
-//   templateUrl: './product-carousel.component.html',
-//   styleUrls: ['./product-carousel.component.css']
-// })
-// export class ProductCarouselComponent implements OnInit {
-//   products: Product[] = [];
-//   currentIndex  = 0;
-//   visibleCount  = 3;
-
-//   constructor(private http: HttpClient) {}
-
-//   ngOnInit() {
-//     this.http.get<Product[]>('http://localhost:3000/products')
-//       .subscribe(data => this.products = data.slice(0, 10));
-//   }
-
-//   prev() {
-//     if (this.currentIndex > 0) {
-//       this.currentIndex--;
-//     }
-//   }
-
-//   next() {
-//     if (this.currentIndex < this.products.length - this.visibleCount) {
-//       this.currentIndex++;
-//     }
-//   }
-
-//   get visibleProducts(): Product[] {
-//     return this.products.slice(
-//       this.currentIndex,
-//       this.currentIndex + this.visibleCount
-//     );
-//   }
-// }
-
 import { Component, ElementRef, AfterViewInit, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
@@ -74,7 +30,7 @@ export class ProductCarouselComponent implements OnInit, AfterViewInit {
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
-      }
+      },
     });
   }
 }
