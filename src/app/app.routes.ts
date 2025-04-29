b31f2165e0c7ea34f8848d2fd7145f31d90e2901
Raul Silveira <raulantsilveira@gmail.com>
2025-04-29 11:12:36 -0300

    feat: create login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import { SearchResultsComponent } from './components/search-results/search-results.component';
-import { HomeComponent } from './components/home/home.component';
+import { SearchResultsComponent } from './pages/search-results/search-results.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'search', component: SearchResultsComponent },
+  { path: 'login', component: LoginComponent }
 ];
