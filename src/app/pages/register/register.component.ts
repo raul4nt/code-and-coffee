52bea80b502ed43a98984ae85e1bfcaf5b4a31ac
Raul Silveira <raulantsilveira@gmail.com>
2025-05-15 15:45:37 -0300

    feat: use setTimeout to redirect the user to the login page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -18,22 +19,51 @@ export class RegisterComponent {
   errorMessage: string = '';
   successMessage: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
-    onRegister() {
-      if (this.password !== this.confirmPassword) {
-        this.errorMessage = 'As senhas não coincidem.'
-        return
-      }
+  onRegister(): void {
+    this.errorMessage = '';
+    this.successMessage = '';
 
-      this.successMessage = 'Cadastro realizado com sucesso!';
-      setTimeout(() => this.router.navigate(['/login']), 2000)
-
+    if (!this.name.trim() || !this.email.trim() || !this.password || !this.confirmPassword) {
+      this.errorMessage = 'Preencha todos os campos.';
+      return;
     }
+
+    if (this.name.trim().split(' ').length < 2) {
+      this.errorMessage = 'Digite seu nome completo.';
+      return;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(this.email)) {
+      this.errorMessage = 'Digite um email válido.';
+      return;
+    }
+
+    if (this.password.length < 6) {
+      this.errorMessage = 'A senha deve ter no mínimo 6 caracteres.';
+      return;
+    }
+
+    if (this.password !== this.confirmPassword) {
+      this.errorMessage = 'As senhas não coincidem.';
+      return;
+    }
+
+    this.authService.register(this.email, this.password).subscribe({
+      next: () => {
+        this.successMessage = 'Cadastro realizado com sucesso! Redirecionando pra página de login...';
+        setTimeout(() => this.router.navigate(['/login']), 5000);
+      },
+      error: (err) => {
+        console.error('Erro ao registrar:', err);
+        this.errorMessage = 'Erro ao registrar. Tente novamente.';
+      }
+    });
+  }
 
   navigateToLogin(): void {
     this.router.navigate(['/login']);
-}
-
-
+  }
 }
