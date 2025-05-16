89ec7048cb99a1747735761ec8176c9f146b0660
Raul Silveira <raulantsilveira@gmail.com>
2025-05-16 17:26:49 -0300

    doc: add comments in header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -18,15 +18,20 @@ export class HeaderComponent implements OnInit, OnDestroy {
   private routerSub!: Subscription
 
   constructor(private router: Router, private authService: AuthService, private el: ElementRef) {}
+  // elementref -> permite acessar elemento raiz do componente e verificar se foi feito um clique dentro ou fora dele
 
   ngOnInit() {
     this.routerSub = this.router.events.subscribe(event => {
+      // listener pra ouvir os eventos
       if (event instanceof NavigationStart) this.searchVisible = false
+      // se o evento for do tipo NavigationStart, ou seja, usuario navegou pra outra pagina, searchVisible vira false
+      // entao se a barra de pesquisa estava ativa ela fica desativada novamente(fecha)
     })
   }
 
   ngOnDestroy() {
     this.routerSub.unsubscribe()
+    // quando evento é destruido, a gente usa unsubscribe pra evitar vazamento de memoria
   }
 
   toggleSearchBar() {
@@ -50,6 +55,9 @@ export class HeaderComponent implements OnInit, OnDestroy {
   onDocumentClick(event: MouseEvent) {
     if (!this.el.nativeElement.contains(event.target) && this.searchVisible) {
       this.searchVisible = false
+    // quando ocorre um evento do tipo click, verifico se a searchVisible esta true(ativa, barra de pesquisa aberta)
+    // e, usando o elementref, se o clique foi na barra de pesquisa ou fora
+    // se foi fora, seto ela como false
     }
   }
 }
