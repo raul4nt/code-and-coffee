c2b79835fe28e112e35bc77b31dd7d76a0aa5a8d
Raul Silveira <raulantsilveira@gmail.com>
2025-07-10 15:27:23 -0300

    create prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   supabaseUrl: 'https://qziqivqmzswcgdabdzdd.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6aXFpdnFtenN3Y2dkYWJkemRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxNjQwMzAsImV4cCI6MjA2Nzc0MDAzMH0.xTOmRFWiJ1cDozWSlnbcMfpGgA_DWDrZEsxllrERWWg'
 };
